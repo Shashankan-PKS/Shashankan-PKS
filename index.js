6a8c13e2e47d8dcfc36799dbc2ea73476f723503
Shashankan-PKS <130852216+Shashankan-PKS@users.noreply.github.com>
2025-05-12 12:12:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -34,6 +34,8 @@ if (document.body.clientWidth < 500) {
     elementsToShow = 1;
 }else if(document.body.clientWidth < 900){
     elementsToShow = 2;
+}else if(document.body.clientWidth >1100){
+    elementsToShow = 4
 }
 
 let sliderContainerWidth = sliderContainer.clientWidth;
