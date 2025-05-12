3e2b36f5ac9cb03d8151c64e00c0cb3c877caff5
Shashankan-PKS <130852216+Shashankan-PKS@users.noreply.github.com>
2025-05-12 12:23:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ if (document.body.clientWidth < 500) {
     elementsToShow = 1;
 }else if(document.body.clientWidth < 900){
     elementsToShow = 2;
-}else if(document.body.clientWidth >1100){
+}else if(document.body.clientWidth < 1400){
     elementsToShow = 4;
 }
 
